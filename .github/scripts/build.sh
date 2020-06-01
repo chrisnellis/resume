0933b727255f65cfb7d0c599116ea414cdd06629
Christopher Nellis <christopher.t.nellis@gmail.com>
2020-05-31 21:44:30 -0400

    Persist output

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,7 +1,5 @@
 npm install -g hackmyresume
-pwd
 cd src/
-pwd
 npm ci
 cp customStylesheet.css node_modules/jsonresume-theme-flat/style.css
 hackmyresume build resume.json to ../index.html -t node_modules/jsonresume-theme-flat
