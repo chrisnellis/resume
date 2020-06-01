b8fa052f168e04714a99fc4b7c372e7ae2e4a151
Christopher Nellis <christopher.t.nellis@gmail.com>
2020-05-31 21:35:44 -0400

    Add statements to verify where we're at

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,5 +1,7 @@
 npm install -g hackmyresume
+pwd
 cd src/
+pwd
 npm ci
 cp customStylesheet.css node_modules/jsonresume-theme-flat/style.css
 hackmyresume build resume.json to ../index.html -t node_modules/jsonresume-theme-flat
