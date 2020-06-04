eb53425e7deea9e67e6ea14d829199269a244c22
Christopher Nellis <christopher.t.nellis@gmail.com>
2020-06-03 22:20:06 -0400

    Create section for PDF file

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -2,5 +2,6 @@ set -x
 npm install -g hackmyresume
 cd src/ || exit
 npm ci
+cp themeCustomizations/mocha-responsive/resume.hbs node_modules/jsonresume-theme-mocha-responsive/resume.hbs
 cp themeCustomizations/mocha-responsive/main.css node_modules/jsonresume-theme-mocha-responsive/styles/main.css
 hackmyresume build resume.json to ../index.html -t node_modules/jsonresume-theme-mocha-responsive
