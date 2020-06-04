0dea9118f363580878a7e1af941ae3ef3be23e63
Christopher Nellis <christopher.t.nellis@gmail.com>
2020-06-03 23:15:34 -0400

    Fix version of wkhtmltopdf and copying script

--- .github/scripts/buildPDF.sh
+++ .github/scripts/buildPDF.sh
@@ -1,10 +1,10 @@
 set -x
-sudo apt-get install -y wkhtmltopdf=0.12.5-1
+sudo apt-get install -y wkhtmltopdf=0.12.4-1
 npm install -g hackmyresume
 cd src/ || exit
 npm ci
 cp themeCustomizations/onepage/resume.hbs node_modules/jsonresume-theme-onepage/resume.hbs
-cp themeCustomizations/onepage/style.css node_modules/jsonresume-theme-onepage/styles/style.css
+cp themeCustomizations/onepage/style.css node_modules/jsonresume-theme-onepage/style.css
 hackmyresume build resume.json to ../pdf.html -t node_modules/jsonresume-theme-onepage
 wkhtmltopdf --print-media-type --no-background --page-size Letter ../pdf.html ../Nellis-Resume.pdf
 rm ../pdf.html
